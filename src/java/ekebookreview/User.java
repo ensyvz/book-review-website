898fc30f2092301e058dedf572e1c8365684bbdd
Enes Yavuz <ensyvz14@gmail.com>
2021-04-29 02:57:59 +0300

    Updated branch with last commit of master.Added basic user bean.

---
+++ src/java/ekebookreview/User.java
@@ -0,0 +1,39 @@
+package ekebookreview;
+import java.security.SecureRandom;
+import java.security.spec.KeySpec;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean(name="User")
+@SessionScoped
+public class User {
+    
+    int id;
+    String name;
+    String username;
+    DBConnection db;
+    
+    public User(int id) {
+        db = new DBConnection();
+        this.id = id;
+        getUserInfo();
+    }
+    private void getUserInfo(){
+        try{
+            db.stmt = db.conn.createStatement();
+            ResultSet results = db.stmt.executeQuery("SELECT name,username FROM users WHERE id="+id);
+            results.next();
+            name = results.getString("name");
+            username = results.getString("username");
+            results.close();
+            db.stmt.close();
+        }
+        catch(SQLException sqlExcept){
+            sqlExcept.printStackTrace();
+        }
+    }
+}
